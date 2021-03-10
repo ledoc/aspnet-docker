c4ac933960a3982d157302a8f5e613a7acd56751
herve <rvfontbonne@hotmail.com>
2021-03-10 14:15:15 +0100

    test1

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace NetCore.Docker
             var max = args.Length != 0 ? Convert.ToInt32(args[0]) : -1;
             while (max == -1 || counter < max)
             {
-                Console.WriteLine($"Counter: {++counter} s");
+                Console.WriteLine($"Counter: {++counter}");
                 await Task.Delay(1000);
             }
         }
